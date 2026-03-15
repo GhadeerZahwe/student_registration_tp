6b46eaf5f2fcb694b159d7ab25d058bd436f1e51
GhadeerZahwe <GhadeerZahwe>
2026-03-15 17:01:13 +0200

    Create Models

--- tp2_project/registration/models.py
+++ tp2_project/registration/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    S_name=models.CharField(max_length=100)
+    S_DOB=models.DateField()
+
+    def __str__(self):
+        return self.S_name
+
+class Course(models.Model):
+    C_name=models.CharField(max_length=100)
+    C_credits=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.C_name
+
+class Register(models.Model):
+    student=models.ForeignKey(Student,on_delete=models.CASCADE)
+    course=models.ForeignKey(Course,on_delete=models.CASCADE)
+    R_date=models.DateField()
+
+    def __str__(self):
+        return f"{self.student} - {self.course}"
